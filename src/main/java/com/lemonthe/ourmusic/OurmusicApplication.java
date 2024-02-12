f0fbbb2e08fb3a9490a01a8390f65334c4de8971
lemonthe <lemonthe777@gmail.com>
2024-02-12 20:57:20 +0300

    Initial commit

---
+++ src/main/java/com/lemonthe/ourmusic/OurmusicApplication.java
@@ -0,0 +1,13 @@
+package com.lemonthe.ourmusic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OurmusicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OurmusicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lemonthe/ourmusic/OurmusicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lemonthe.ourmusic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OurmusicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
