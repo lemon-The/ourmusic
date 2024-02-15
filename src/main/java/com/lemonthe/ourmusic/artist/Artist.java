76fe4ac86172242f5c84d992d0fec1a90a495411
lemonthe <lemonthe777@gmail.com>
2024-02-15 22:09:07 +0300

    Create Artist entity

--- src/main/java/com/lemonthe/ourmusic/artist/Artist.java
+++ src/main/java/com/lemonthe/ourmusic/artist/Artist.java
@@ -1,15 +1,24 @@
 package com.lemonthe.ourmusic.artist;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 /**
  * Artist
  */
+@Entity
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Artist {
 
   @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
   private Long id;
-  private final String name;
+  private String name;
 }
