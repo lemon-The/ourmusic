b703a2ff349adf307efaef3279cf8bd489eea4ae
lemonthe <lemonthe777@gmail.com>
2024-02-16 20:22:02 +0300

    Complete album controller and create album creation dto

--- src/main/java/com/lemonthe/ourmusic/album/AlbumController.java
+++ src/main/java/com/lemonthe/ourmusic/album/AlbumController.java
@@ -1,8 +1,25 @@
 package com.lemonthe.ourmusic.album;
 
+import java.net.URI;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import com.lemonthe.ourmusic.album.dto.AlbumCreationDTO;
+
+import jakarta.validation.Valid;
 
 /**
  * AlbumController
@@ -14,5 +31,44 @@ public class AlbumController {
   @Autowired
   private AlbumService albumService;
 
+  @GetMapping
+  public ResponseEntity<List<Album>> getAlbums() {
+    List<Album> albums = albumService.getAlbums();
+    if (albums.isEmpty()) {
+      throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+    }
+    return ResponseEntity.ok(albums);
+  }
+
+  @GetMapping("/{id}")
+  public ResponseEntity<Album> getAlbum(@PathVariable Long id) {
+    return ResponseEntity.ok(albumService
+        .getAlbum(id)
+        .orElseThrow(() -> {
+          throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        }));
+  }
+
+  @RequestMapping(method = {RequestMethod.POST, RequestMethod.PUT})
+  public ResponseEntity<?> createAlbum(
+      @Valid @RequestBody AlbumCreationDTO albumDTO,
+      Errors errors) {
+    if (errors.hasErrors()) {
+      return ResponseEntity.badRequest().body(errors);
+    }
+    Album album = albumService.save(albumDTO.asAlbum());
+    URI location = ServletUriComponentsBuilder
+        .fromCurrentRequest()
+        .path("/{id}")
+        .buildAndExpand(album.getId())
+        .toUri();
+    return ResponseEntity.created(location).build();
+  }
+
+  @DeleteMapping("/delete/{id}")
+  public ResponseEntity<?> deleteAlbum(@PathVariable Long id) {
+    albumService.delete(id);
+    return ResponseEntity.noContent().build();
+  }
   
 }
---
+++ src/main/java/com/lemonthe/ourmusic/album/dto/AlbumCreationDTO.java
@@ -0,0 +1,17 @@
+package com.lemonthe.ourmusic.album.dto;
+
+import com.lemonthe.ourmusic.album.Album;
+
+import jakarta.validation.constraints.NotBlank;
+
+/**
+ * AlbumCreationDTO
+ */
+public record AlbumCreationDTO(
+    Long id,
+    @NotBlank String title) {
+
+  public Album asAlbum() {
+    return new Album(id, title);
+  }
+}
