2f87e2ff7af3c96de8d169164e0d90eb4bfa6ce3
lemonthe <lemonthe777@gmail.com>
2024-02-23 15:34:50 +0300

    Remove unused method from AudioDataRepository

--- src/main/java/com/lemonthe/ourmusic/scraper/AudioDataRepository.java
+++ src/main/java/com/lemonthe/ourmusic/scraper/AudioDataRepository.java
@@ -1,11 +1,9 @@
 package com.lemonthe.ourmusic.scraper;
 
 import java.util.List;
-import java.util.Optional;
 
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 /**
@@ -13,11 +11,6 @@ import org.springframework.stereotype.Repository;
  */
 @Repository
 public interface AudioDataRepository extends JpaRepository<AudioData, Long> {
-
-	@Query("SELECT a "
-			+ "FROM AudioData a "
-			+ "WHERE a.searchQuery = ?1")
-	List<AudioData> findAllBySearchQuery(String query);
 
 	List<AudioData> findBySearchQueryAndWebsiteName(String query, String websiteName, Pageable pageable);
 
